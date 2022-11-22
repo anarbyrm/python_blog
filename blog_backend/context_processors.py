cacd1190342f5917724a87e54595ab62b270b6b9
Anar Bairam <anar.b@labrin.net>
2022-11-22 15:33:57 +0400

    filter column added

--- blog_backend/context_processors.py
+++ blog_backend/context_processors.py
@@ -1,9 +1,9 @@
-from blog.models import WebsiteSetting, Info
+from blog.models import WebsiteSetting, Info, Tag
 
 
 def custom_context(request):
     context = dict()
     context['setting'] = WebsiteSetting.objects.last()
     context['info'] = Info.objects.last()
-
+    context['tags'] = Tag.objects.all()
     return context
