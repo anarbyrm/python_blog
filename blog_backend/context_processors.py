7eabad2af3d5d45af692583cfbdacb11059eade8
Anar Bairam <anar.b@labrin.net>
2022-11-23 17:35:58 +0400

    refactoring

--- blog_backend/context_processors.py
+++ blog_backend/context_processors.py
@@ -1,4 +1,4 @@
-from blog.models import WebsiteSetting, Info, Tag
+from blog.models import WebsiteSetting, Info, Tag, Post
 
 
 def custom_context(request):
@@ -6,4 +6,5 @@ def custom_context(request):
     context['setting'] = WebsiteSetting.objects.last()
     context['info'] = Info.objects.last()
     context['tags'] = Tag.objects.all()
+    context['all_posts_count'] = Post.objects.count()
     return context
