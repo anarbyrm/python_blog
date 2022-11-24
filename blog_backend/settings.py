c1234a7af1a6c6e52275fb496a42a7ad0169c9e1
Anar Bairam <anar.b@labrin.net>
2022-11-24 14:53:45 +0400

    final configs for deployment

--- blog_backend/settings.py
+++ blog_backend/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure--9h9(qq&^h*a7pb!qnhx&v8byw=zly-i@vguefx&9z0$!_jj8^"
+SECRET_KEY = str(os.environ.get('SECRET_KEY', "django-insecure--9h9(qq&^h*a7pb!qnhx&v8byw=zly-i@vguefx&9z0$!_jj8^"))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -92,11 +92,16 @@ WSGI_APPLICATION = "blog_backend.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-    }
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get('POSTGRES_DB'),
+        "USER": os.environ.get('POSTGRES_USER'),
+        "PASSWORD": os.environ.get('POSTGRES_PASSWORD'),
+        "HOST": os.environ.get('POSTGRES_HOST'),
+        "PORT": os.environ.get('POSTGRES_PORT'),
+    },
 }
 
 
@@ -135,7 +140,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
-STATICFILES_DIRS = [ BASE_DIR / "static" ]
+STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = "static_root"
 
 MEDIA_URL = '/media/'
